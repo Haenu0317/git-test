d65ba6ac46932eca8259a1e36cc037f0e06a8a1f
Haenu <2523053533@qq.com>
2023-01-24 15:23:02 +0800

    master commit2

--- src/Temp.java
+++ src/Temp.java
@@ -6,6 +6,7 @@ public class Temp {
         System.out.println("hello world");
         System.out.println("hello world3");
         System.out.println("hello world4");
+        System.out.println("master test");
 
 
     }
